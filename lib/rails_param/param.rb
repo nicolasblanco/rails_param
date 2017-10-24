d94b040eff7bf8caa416aa582cb4779d96bcd853
Carlos Figueiredo <carlos.figueiredo@koolicar.com>
2017-10-24 15:49:41 -0400

    Refactor coerse method for :booleans

--- lib/rails_param/param.rb
+++ lib/rails_param/param.rb
@@ -101,7 +101,12 @@ module RailsParam
         return DateTime.parse(param) if type == DateTime
         return Array(param.split(options[:delimiter] || ",")) if type == Array
         return Hash[param.split(options[:delimiter] || ",").map { |c| c.split(options[:separator] || ":") }] if type == Hash
-        return (/^(false|f|no|n|0)$/i === param.to_s ? false : (/^(true|t|yes|y|1)$/i === param.to_s ? true : (raise ArgumentError))) if type == TrueClass || type == FalseClass || type == :boolean
+        if type == TrueClass || type == FalseClass || type == :boolean
+          return false if /^(false|f|no|n|0)$/i === param.to_s
+          return true if /^(true|t|yes|y|1)$/i === param.to_s
+
+          raise ArgumentError
+        end
         if type == BigDecimal
           param = param.delete('$,').strip.to_f if param.is_a?(String)
           return BigDecimal.new(param, (options[:precision] || DEFAULT_PRECISION))
