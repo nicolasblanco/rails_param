0ee364687fcd0d62b9817dd2f3d42809241bd663
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-12-20 13:51:57 -0500

    boolean constants

--- lib/rails_param/coercion/boolean_param.rb
+++ lib/rails_param/coercion/boolean_param.rb
@@ -4,13 +4,18 @@ module RailsParam
       class BooleanParam
         attr_reader :param, :options, :type
 
+        FALSEY = /^(false|f|no|n|0)$/i.freeze
+        TRUTHY = /^(true|t|yes|y|1)$/i.freeze
+
         def initialize(param:, options: nil, type: nil)
-          @param, @options, @type = param, options, type
+          @param = param
+          @options = options
+          @type = type
         end
 
         def coerce
-          return false if /^(false|f|no|n|0)$/i === param.to_s
-          return true if /^(true|t|yes|y|1)$/i === param.to_s
+          return false if FALSEY === param.to_s
+          return true if TRUTHY === param.to_s
 
           raise ArgumentError
         end
