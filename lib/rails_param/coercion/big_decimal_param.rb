c8a77f2b1bb0916c07323f1ccef5cca98505352d
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-12-20 13:43:27 -0500

    clean up

--- lib/rails_param/coercion/big_decimal_param.rb
+++ lib/rails_param/coercion/big_decimal_param.rb
@@ -7,17 +7,19 @@ module RailsParam
         DEFAULT_PRECISION = 14
 
         def initialize(param:, options: nil, type: nil)
-          @param, @options, @type = param, options, type
+          @param = param
+          @options = options
+          @type = type
         end
 
         def coerce
-          stripped_param =  if param.is_a?(String)
-                        param.delete('$,').strip.to_f
-                      else
-                        param
-                      end
+          stripped_param = if param.is_a?(String)
+                             param.delete('$,').strip.to_f
+                           else
+                             param
+                           end
 
-          BigDecimal(stripped_param, (options[:precision] || DEFAULT_PRECISION))
+          BigDecimal(stripped_param, options[:precision] || DEFAULT_PRECISION)
         end
       end
     end
