74e9b0cc36db9d24eeb7746cf0f858e4b9bdebf7
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-12-20 17:14:27 -0500

    clean up time param

--- lib/rails_param/coercion/time_param.rb
+++ lib/rails_param/coercion/time_param.rb
@@ -6,19 +6,16 @@ module RailsParam
 
         TIME_TYPES = [Date, DateTime, Time].freeze
 
-
         def initialize(param:, options: nil, type: nil)
-          @param, @options, @type = param, options, type
+          @param = param
+          @options = options
+          @type = type
         end
 
         def coerce
-          if TIME_TYPES.include? type
-            if options[:format].present?
-              return type.strptime(param, options[:format])
-            else
-              return type.parse(param)
-            end
-          end
+          return type.strptime(param, options[:format]) if options[:format].present?
+
+          type.parse(param)
         end
       end
     end
