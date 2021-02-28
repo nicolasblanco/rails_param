b138d94ee66dd67f5906d2224780010e5181b02f
Tymm Schmitke <ifellinaholeonce@gmail.com>
2021-02-27 21:40:45 -0500

    initial validator spec

---
+++ spec/rails_param/validator_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'rails_param/validator'
+
+describe RailsParam::Param::Validator do
+  let(:name)    { "foo" }
+  let(:value)   { "bar" }
+  let(:options) { { required: true } }
+  let(:type)    { String }
+  let(:parameter) do
+    RailsParam::Param::Parameter.new(
+      name: name,
+      value: value,
+      options: options,
+      type: type
+    )
+  end
+
+  subject { described_class.new(parameter) }
+
+  let(:validator_class)  { RailsParam::Param::Validator::Required }
+  let(:validator_double) { double }
+
+  before :each do
+    allow(validator_double).to receive(:valid!)
+    allow(validator_class).to receive(:new).and_return(validator_double)
+  end
+
+  describe "#validate!" do
+    it "initializes a validator class based on the provided option" do
+      subject.validate!
+
+      expect(validator_class).to have_received(:new).with(parameter)
+    end
+  end
+end
